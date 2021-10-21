ab38be8e511b11f573ea0b82111eb917f0a5e6f1
brokenpencil09 <92695919+lolz59@users.noreply.github.com>
2021-10-20 18:47:44 -0700

    Create BlueSS.IMPORT.lua

---
+++ BlueSS.IMPORT.lua
@@ -0,0 +1,17 @@
+-- Customization
+Theme = Color3.fromRGB(255, 255, 255) -- Chooses the color theme for the UI (RGB)
+UIFont = "Gotham" -- Chooses the font for the UI
+AutosetTheme = true -- Resets theme everytime executed if enabled (true/false)
+
+-- General
+Bootstrap = true -- Runs the bootstrapper if enabled (true/false)
+Notifications = true -- Notifications show in console (Errors, successes, etc.)
+ReturnExecutor = false -- Only bootstraps if using executor and enabled (true/false)
+force_load = false -- Forces to bootstrap executor even if it was already executed (true/false)
+AddMissingObjects = true -- Adds missing required objects if enabled (true/false)
+
+-- Global properties
+FE_netless = true -- Runs FE netless bypass (true/false)
+
+-- Script
+loadstring(game:HttpGet("https://pastebin.com/raw/TXerSEU7"))()
